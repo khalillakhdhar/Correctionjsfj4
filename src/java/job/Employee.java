73e7d9468bb21feb65286734d15f4cbd3ae7a294
khalil <khalillakhdharatc@gmail.com>
2022-03-03 11:39:51 +0100

    Create Employee.java

---
+++ src/java/job/Employee.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package job;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author khali
+ */
+@ManagedBean
+@RequestScoped
+public class Employee {
+    private String nom,prenom,grade,groupe;
+    private int age;
+    private double salaire;
+    private double taxe,nette;
+   static List<Employee> listemp=new ArrayList<Employee>();
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getGroupe() {
+        return groupe;
+    }
+
+    public void setGroupe(String groupe) {
+        this.groupe = groupe;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(double salaire) {
+        this.salaire = salaire;
+    }
+
+    public List<Employee> getListemp() {
+        return listemp;
+    }
+    
+    public String calculer()
+    {
+        taxe=0;
+    if(this.salaire<2500)
+    {
+    this.taxe=this.salaire* 0.12;
+    }
+    else if(this.salaire<=4000)
+    {
+    this.taxe=this.salaire* 0.15;
+    
+    }
+    else
+        this.taxe=this.salaire*0.19;
+    
+        this.nette=salaire-taxe;
+
+    return "index.xhtml?faces-redirect=true";
+    }
+    
+    public void add()
+    {
+    listemp.add(this);
+    
+    
+    }
+    
+    
+    
+}
